232483d071e029cd16e232fd49405b0f4a570c1a
Robert Johansson <swerobo@gmail.com>
2024-06-19 07:31:48 +0200

    Initial Commit

--- HamburgerMenu/App.xaml.cs
+++ HamburgerMenu/App.xaml.cs
@@ -1,13 +1,42 @@
-﻿using System.Configuration;
+﻿using DevExpress.Xpf.Core;
+using hamburgermenu.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
 using System.Data;
+using System.Linq;
 using System.Windows;
 
-namespace HamburgerMenu;
-
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
-public partial class App : Application
+namespace hamburgermenu
 {
-}
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private IHost? _host;
+        public App()
+        {
+            CompatibilitySettings.UseLightweightThemes = true;
 
+            _host = Host.CreateDefaultBuilder()
+            .ConfigureServices((context, services) =>
+            {
+                services.AddSingleton<MainWindow>();
+                services.AddSingleton<MainViewModel>();
+            })
+            .Build();
+
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await _host!.StartAsync();
+            var mainWindow = _host.Services.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+            base.OnStartup(e);
+        }
+    }
+}
